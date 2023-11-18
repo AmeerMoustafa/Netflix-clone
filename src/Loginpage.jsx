6f4fc230fe4b2dd751c871784073b3c09e57ba13
Ameer-M <t3hdamnedone@gmail.com>
2023-11-18 22:20:14 +0200

    Implementing hero section

--- src/Loginpage.jsx
+++ src/Loginpage.jsx
@@ -1,5 +1,19 @@
+import "../src/styles/utilities.css";
+import "../src/styles/loginpage.css";
+
 function Loginpage() {
-  return <h1>Hello Group</h1>;
+  return (
+    <div className="flex-col hero-parent">
+      <nav className="flex navbar-parent">
+        <div className="navbar-logo-container">
+          <img src="../src/assets/loginpage/netflixlogo.svg" />
+        </div>
+        <div>
+          <button>Sign in</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Loginpage;
