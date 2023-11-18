3246073ed48249e903bd6b0c31a56dc8281f999d
Warden <samer.charaf3@gmail.com>
2023-11-18 23:32:19 +0200

    Finished HTML of footer

---
+++ src/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Questions? Contact us.</p>
+      <div className="footer-columns">
+        <ul>
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+          <li>
+            <a href="#">Investor Relations</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+          <li>
+            <a href="#">Speed Test</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Help Center</a>
+          </li>
+          <li>
+            <a href="#">Jobs</a>
+          </li>
+          <li>
+            <a href="#">Cookie Preferences</a>
+          </li>
+          <li>
+            <a href="#">Legal Notices</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Account</a>
+          </li>
+          <li>
+            <a href="#">Ways to Watch</a>
+          </li>
+          <li>
+            <a href="#">Corporate Information</a>
+          </li>
+          <li>
+            <a href="#">Only on Netflix</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Media Center</a>
+          </li>
+          <li>
+            <a href="#">Terms of Use</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
