21451671d291da4845010d35cf57030125c4d87d
Warden <samer.charaf3@gmail.com>
2023-11-19 01:03:40 +0200

    Finished styling the navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Loginpage from "./Loginpage.jsx";
 import Footer from "./Footer.jsx";
+import Navbar from "./Navbar.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <Loginpage />
     <Footer />
   </React.StrictMode>
