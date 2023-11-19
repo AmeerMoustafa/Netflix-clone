5791a4358b6e44961f87a8dd217b4b3b0957e326
Your Name <your_email@example.com>
2023-11-19 14:41:31 +0200

    Merge branch 'development' of github.com:AmeerMoustafa/Netflix-clone into development

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,5 +1,10 @@
+import Navbar from "../components/reusable/Navbar";
+import Footer from "../components/reusable/Footer";
 function Loginpage() {
-  return <div className="hero-container-parent"></div>;
+  return <div className="hero-container-parent">
+    <Navbar />
+    <Footer />
+  </div>;
 }
 
 export default Loginpage;
