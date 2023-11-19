5d08c2b8cea95a0069d0ea4d6bf44926fc166eb0
Ameer-M <t3hdamnedone@gmail.com>
2023-11-19 16:29:52 +0200

    Refactored some sections into components.

---
+++ src/components/loginpage/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+  return (
+    <div className="flex hero-form-container">
+      <input type="text" placeholder="Email address" />
+      <button>{"Get Started >"}</button>
+    </div>
+  );
+}
+
+export default Signup;
--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,39 +1,35 @@
 import "../styles/utilities.css";
 import "../styles/loginpage.css";
+import Signup from "../components/loginpage/Signup";
+import LoginNav from "../components/loginpage/LoginNav";
 
 function Loginpage() {
   return (
-    <div className="flex-col hero-parent">
-      <nav className="flex navbar-parent">
-        <div className="navbar-logo-container">
-          <img src="../src/assets/loginpage/netflixlogo.svg" />
-        </div>
-        <div className="flex navbar-signin-container">
-          <select>
-            <option>English</option>
-            <option>العربية</option>
-          </select>
-          <button>Sign in</button>
-        </div>
-      </nav>
+    <>
+      {/* Hero section */}
+      <section className="flex-col hero-parent">
+        <LoginNav />
+        <div className="flex-col hero-content-container">
+          <h1>Unlimited Movies, TV shows, and more</h1>
+          <span>Watch anywhere. Cancel anytime.</span>
 
-      <div className="flex-col hero-content-container">
-        <h1>Unlimited Movies, TV shows, and more</h1>
-        <span>Watch anywhere. Cancel anytime.</span>
+          <div className="flex-col hero-signup-container">
+            <span>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </span>
 
-        <div className="flex-col hero-signup-container">
-          <span>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </span>
-
-          <div className="flex hero-form-container">
-            <input type="text" placeholder="Email address" />
-            <button>{"Get Started >"}</button>
+            <Signup />
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+      {/* Features section */}
+
+      <section className="tv-feature-parent">
+        <div className="tv-feature-child"></div>
+      </section>
+    </>
   );
 }
 
