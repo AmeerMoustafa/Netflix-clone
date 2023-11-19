cdfd8b10567d8834a4db007fc890f273bb95e5f6
Ameer-M <t3hdamnedone@gmail.com>
2023-11-19 14:02:32 +0200

    Merged from develop to mainpage

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,5 +1,40 @@
+import "../src/styles/utilities.css";
+import "../src/styles/loginpage.css";
+
 function Loginpage() {
-  return <div className="hero-container-parent"></div>;
+  return (
+    <div className="flex-col hero-parent">
+      <nav className="flex navbar-parent">
+        <div className="navbar-logo-container">
+          <img src="../src/assets/loginpage/netflixlogo.svg" />
+        </div>
+        <div className="flex navbar-signin-container">
+          <select>
+            <option>English</option>
+            <option>العربية</option>
+          </select>
+          <button>Sign in</button>
+        </div>
+      </nav>
+
+      <div className="flex-col hero-content-container">
+        <h1>Unlimited Movies, TV shows, and more</h1>
+        <span>Watch anywhere. Cancel anytime.</span>
+
+        <div className="hero-signup-container">
+          <span>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </span>
+
+          <div className="flex hero-form-container">
+            <input type="text" />
+            <button>{"Get Started >"}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Loginpage;
