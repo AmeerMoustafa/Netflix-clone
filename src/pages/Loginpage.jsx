b34ad44e5fffd2308661c6f900e43d00ca25c407
Ameer-M <t3hdamnedone@gmail.com>
2023-11-19 18:21:30 +0200

    Implement TV and download feature sections

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -26,8 +26,50 @@ function Loginpage() {
 
       {/* Features section */}
 
-      <section className="tv-feature-parent">
-        <div className="tv-feature-child"></div>
+      {/* TV Features section */}
+      <section className="flex feature-parent">
+        <div className="flex feature-child">
+          <div className="flex-col feature-content-container">
+            <h2>Enjoy on your TV</h2>
+            <p>
+              Watch on Smart TVs, Playstation, Xbox, Chromecast , Apple TV,
+              Blu-ray players, and more.
+            </p>
+          </div>
+          <div className="tv-image-container">
+            <img className="tv-image" src="./src/assets/loginpage/tv.png" />
+            <video
+              className="tv-video"
+              src="./src/assets/loginpage/tv-video.m4v"
+              autoPlay
+              loop
+              muted
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* downloads Features section */}
+      <section className="flex feature-parent">
+        <div className="flex feature-child">
+          <div className="download-image-container">
+            <img src="./src/assets/loginpage/downloading-image.jpg" />
+            <div className="flex download-overlay-container">
+              <img src="./src/assets/loginpage/downloads-boxshot.png" />
+              <div className="flex-col download-overlay-content">
+                <span className="overlay-title">Stranger Things</span>
+                <span className="overlay-subtitle">Downloading...</span>
+              </div>
+              <img src="./src/assets/loginpage/download-icon.gif" />
+            </div>
+          </div>
+          <div className="flex-col downloads-content-container feature-content-container">
+            <h2>Download your shows to watch offline</h2>
+            <p>
+              Save your favorites easily and always have something to watch.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
