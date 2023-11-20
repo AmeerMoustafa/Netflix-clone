67de52be7b395659dea43f8c26243909027143c8
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-20 20:28:18 +0200

    edited App.jsx file so that I can merge safely now

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Loginpage from "./pages/Loginpage";
 import Moviepage from "./pages/Moviepage";
 
 function App() {
-  return <Moviepage />;
+  return <Loginpage></Loginpage>;
 }
 
 export default App;
