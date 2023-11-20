8b79a20b69a3504fcf4fb7c85d8caf26bd290ea2
Your Name <your_email@example.com>
2023-11-20 23:19:37 +0200

    Update MoviePage

--- src/components/moviepage/MovieDescription.jsx
+++ src/components/moviepage/MovieDescription.jsx
@@ -1,23 +1,25 @@
 import "../../styles/utilities.css";
 import "../../styles/moviepage.css";
 
-const MovieDescription = () => {
+const MovieDescription = ({ movie }) => {
+  const changeBackground = {
+    backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.poster_path})`,
+  };
   return (
     <div className="moviedescription">
-      <div className="hero flex">
+      <div className="hero flex" style={changeBackground}>
         <div className="hero-container flex-col">
           <div className="hero-details">
-            <img src="src\assets\moviepage\hero-logo.png" alt="Movie Logo" />
-            <h2 className="hero-title">Scarface</h2>
+            <img
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt="Movie Logo"
+            />
+            <h2 className="hero-title">{movie.original_title}</h2>
             <p className="hero-info grey">
               1983 | <span className="hero-maturity">18+</span> | 2h 49m |
               Thrillers
             </p>
-            <p className="hero-description">
-              In a ruthless rise to Miami drug lord, a Cuban-born gangster
-              descends into addiction, obsession and brutality, with grisly
-              consequences.
-            </p>
+            <p className="hero-description">{movie.overview}</p>
             <p className="hero-starring">
               <span className="grey">Starring: </span> Al Pacino, Steven Bauer,
               Michelle Pfeiffer
--- src/pages/Moviepage.jsx
+++ src/pages/Moviepage.jsx
@@ -7,25 +7,38 @@ import ComingSoon from "../components/moviepage/ComigSoon";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-// const apiKey =
-//   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTFjZGY1YmYzNTdjMTJjMzNmMGRkNTc1MDU4YTI1YyIsInN1YiI6IjY1NThmM2U4YjU0MDAyMTRjZjM5ZjEzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GRAJBNXxK0rODxMyrd9a7IxiyJonE0jpKGUPC6MbrSw";
-// const url = `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`;
-// const options = {
-//   method: "GET",
-//   headers: { accept: "application/json", Authorization: apiKey },
-// };
 
-// fetch(url, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.error("error:" + err));
+
 
 function Moviepage() {
-  const { movieId } = useParams();
-  console.log(movieId);
+  const { movieID } = useParams();
+  const [movie , setMovie] = useState({})
+
+  const fetchData = () => {
+    const apiKey =
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTFjZGY1YmYzNTdjMTJjMzNmMGRkNTc1MDU4YTI1YyIsInN1YiI6IjY1NThmM2U4YjU0MDAyMTRjZjM5ZjEzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GRAJBNXxK0rODxMyrd9a7IxiyJonE0jpKGUPC6MbrSw";
+    const url = `https://api.themoviedb.org/3/movie/${movieID}?language=en-US`;
+    const options = {
+      method: "GET",
+      headers: { accept: "application/json", Authorization: apiKey },
+    };
+  
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((json) => {
+        setMovie(json)
+        console.log(json);
+      })
+      .catch((err) => console.error("error:" + err));
+  };
+  
+useEffect(() => {
+  fetchData()
+} , []);
+
   return (
     <div className="moviepage">
-      <MovieDescription></MovieDescription>
+      <MovieDescription movie={movie}></MovieDescription>
       <MoreDetails></MoreDetails>
       <MoreLikeThis></MoreLikeThis>
       <ComingSoon></ComingSoon>
