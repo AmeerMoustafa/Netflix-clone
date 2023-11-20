dc5b723df50e300e410f0d9c672a000491ac0f13
fadelBayloun <fadel.bayloun12@gmail.com>
2023-11-20 19:34:28 +0200

    added more like this section and coming soon section

--- src/pages/Moviepage.jsx
+++ src/pages/Moviepage.jsx
@@ -2,12 +2,15 @@ import "../styles/utilities.css";
 import "../styles/moviepage.css";
 import MovieDescription from "../components/moviepage/MovieDescription";
 import MoreDetails from "../components/moviepage/MoreDetails";
+import MoreLikeThis from "../components/moviepage/MoreLikeThis";
+import ComingSoon from "../components/moviepage/ComigSoon";
 
 function Moviepage() {
   return (
     <div className="moviepage">
       <MovieDescription></MovieDescription>
-      <MoreDetails></MoreDetails>
+      <MoreLikeThis></MoreLikeThis>
+      <ComingSoon></ComingSoon>
     </div>
   );
 }
