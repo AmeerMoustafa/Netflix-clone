8c2d9248ba5710a757d6936dba14c26718b292c3
Ameer-M <t3hdamnedone@gmail.com>
2023-11-21 23:16:44 +0200

    Fixed more movie page styling

--- src/components/moviepage/MoreLikeThis.jsx
+++ src/components/moviepage/MoreLikeThis.jsx
@@ -1,15 +1,20 @@
 import "../../styles/utilities.css";
 import "../../styles/moviepage.css";
-import { Link } from "react-router-dom";
 
-const MoreLikeThis = ({recomArray , handleClick}) => {
+const MoreLikeThis = ({ recomArray, handleClick }) => {
   return (
     <div className="morelikethis">
       <div className="morelikethis-title">
         <h2>More Like This</h2>
         <div className="morelikethis-container flex">
-          {recomArray.map((movie) => (
-              <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} onClick={() => {handleClick(movie.id)}}/>
+          {recomArray.map((movie, i) => (
+            <img
+              key={i}
+              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              onClick={() => {
+                handleClick(movie.id);
+              }}
+            />
           ))}
         </div>
       </div>
