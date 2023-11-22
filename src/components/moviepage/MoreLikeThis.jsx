f293d71d2fac9fa541a4d98ce3accb99eb705afc
Your Name <your_email@example.com>
2023-11-22 19:56:39 +0200

    Fix Movie Details Issues

--- src/components/moviepage/MoreLikeThis.jsx
+++ src/components/moviepage/MoreLikeThis.jsx
@@ -7,15 +7,17 @@ const MoreLikeThis = ({ recomArray, handleClick }) => {
       <div className="morelikethis-title">
         <h2>More Like This</h2>
         <div className="morelikethis-container flex">
-          {recomArray.map((movie, i) => (
-            <img
-              key={i}
-              src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-              onClick={() => {
-                handleClick(movie.id);
-              }}
-            />
-          ))}
+          {recomArray.length !== 0
+            ? recomArray.map((movie, i) => (
+                <img
+                  key={i}
+                  src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                  onClick={() => {
+                    handleClick(movie.id);
+                  }}
+                />
+              ))
+            : <div className="red">No Recommended Movies</div>}
         </div>
       </div>
     </div>
